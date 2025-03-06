cf591510e99471b675b67e0c194215fa827bc4bf
jhulett18 <jonathan_hulett@ymail.com>
2025-03-06 17:34:37 -0500

    original

--- main/vite.config.ts
+++
@@ -1,7 +0,0 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-
-export default defineConfig({
-  plugins: [react()],
-  base: 'https://github.com/jhulett18/InsuranceLeadGenerator-Demo.git', // Replace with your actual repository name
-})
